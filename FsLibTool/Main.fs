1b65843b3cb551bd1c4c38d2c8521bdc4cb26487
Vesa Karvonen <vesa.a.j.k@gmail.com>
2014-05-31 13:37:52 +0300

    Work-in-progress commit to transfer work to another machine.

---
+++ FsLibTool/Main.fs
@@ -0,0 +1,25 @@
+﻿open System
+open System.IO
+
+type LibInfo = {
+    LibDir: string
+    LibName: string
+    DocFile: string
+  }
+
+[<EntryPoint>]
+let main argv =
+  let libs =
+    Directory.EnumerateDirectories "Libs"
+    |> Seq.filter (fun libDir ->
+       Directory.EnumerateFiles (libDir, "*.fsi")
+       |> Seq.isEmpty |> not)
+    |> Seq.map (fun libDir ->
+       let libName = Path.GetFileName libDir
+       let docFile = Path.Combine ("Docs", libName + ".html")
+       {LibDir = libDir
+        LibName = libName
+        DocFile = docFile})
+    |> List.ofSeq
+  printf "%A" libs
+  0
