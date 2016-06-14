af446fcb1704543b986e5627baee0ed81211c518
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-06-14 07:37:10 +0300

    Simplified

--- src/Main.fs
+++ src/Main.fs
@@ -15,39 +15,37 @@ let help () =
     ^ Path.GetFileName exePath
   1
 
-let go options =
-  match options with
-   | {out = Some out
-      name = Some name
-      icon = icon
-      files = files} when not ^ List.isEmpty files ->
-     Directory.CreateDirectory out |> ignore
+let go = function
+  | {out = Some out
+     name = Some name
+     icon = icon
+     files = files} when not ^ List.isEmpty files ->
+    Directory.CreateDirectory out |> ignore
 
-     let exeDir = Path.GetDirectoryName exePath
-     for file in ["fs-libtool.js"; "fs-libtool.css"] do
-       File.Copy (Path.Combine (exeDir, file), Path.Combine (out, file), true)
+    let exeDir = Path.GetDirectoryName exePath
+    for file in ["fs-libtool.js"; "fs-libtool.css"] do
+      File.Copy (Path.Combine (exeDir, file), Path.Combine (out, file), true)
 
-     let docFile = Path.Combine (out, name + ".html")
+    let docFile = Path.Combine (out, name + ".html")
 
-     use wr = new StreamWriter (docFile)
-     RefGen.generate wr name icon files
+    use wr = new StreamWriter (docFile)
+    RefGen.generate wr name icon files
 
-     0
-   | _ ->
-     help ()
+    0
+  | _ ->
+    help ()
 
-let rec parseOptions options args =
-  match args with
-   | "--out" :: dir :: args ->
-     parseOptions {options with out = Some dir} args
-   | "--name" :: name :: args ->
-     parseOptions {options with name = Some name} args
-   | "--icon" :: icon :: args ->
-     parseOptions {options with icon = Some icon} args
-   | "--" :: args ->
-     go {options with files = args}
-   | _ ->
-     help ()
+let rec parseOptions options = function
+  | "--out" :: dir :: args ->
+    parseOptions {options with out = Some dir} args
+  | "--name" :: name :: args ->
+    parseOptions {options with name = Some name} args
+  | "--icon" :: icon :: args ->
+    parseOptions {options with icon = Some icon} args
+  | "--" :: args ->
+    go {options with files = args}
+  | _ ->
+    help ()
 
 [<EntryPoint>]
 let main argv =
